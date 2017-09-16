05cf8aa2b063c27a43ad608eaf58eee9b4a11f2f
Calder White <calderwhite1@gmail.com>
2017-09-16 09:51:24 -0400

    Added fb messanger & gmail front end scraping.

--- extension/content.js
+++ extension/content.js
@@ -1,2 +1,33 @@
 // this code runs on every page opened
+function scrapeFacebookMessanger(){
+  var messages = [];
+  var chat = document.getElementsByClassName("_aok");
+  // find the messages not from the current user.
+  var last = true;
+  for (var i = 0;i<chat.length; i++) {
+    if(chat[i].parentElement.dataset.tooltipPosition === "left"){
+      if(last){
+        messages.push([chat[i].textContent]);
+      } else{
+        messages[messages.length-1].push(chat[i].textContent);
+      }
+      last = false;
+    } else{
+      last = true;
+    }
+  };
+  return {"messages":messages};
+}
+function scrapeGmail(){
+  const from = document.getElementsByClassName("gD")[0].dataset.hovercardId;
+  const subject = document.getElementsByClassName("hP")[0].textContent;
+  const content = document.getElementById(":ey").textContent;
+  const pkg = {
+    "from" : from,
+    "subject" : subject,
+    "messages" : [content]
+  }
+  return pkg
+}
+
 console.log("Hello World from content.")
