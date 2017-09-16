bdaff170b03149b9d6b91b04c7a396769c154f9c
matthewjmay <mmay51598@gmail.com>
2017-09-16 12:26:04 -0400

    fixed file structure

---
+++ extension/src/background.js
@@ -0,0 +1,19 @@
+// this code can run in the background, but is currently set to work with events.
+// Google: chrome.runtime.onMessage.addListener
+
+const ENDPOINT = 'https://api.cymon.io/v2';
+const USERNAME = 'htn2017';
+const PASSWD = 'hackthenorth';
+
+let myHeaders = new Headers();
+
+const authConfig = {
+  method: 'POST',
+  body: {
+    username: USERNAME,
+    password: PASSWD
+  },
+  headers: new Headers()
+};
+
+fetch(ENDPOINT + '/auth/login', authConfig);
--- extension/src/content.js
+++ extension/src/content.js
@@ -1,6 +1,3 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
-
 // this code runs on every page opened
 function scrapeFacebookMessanger() {
   var messages = [];
@@ -42,5 +39,3 @@ var index = {
   "mail.google.com": "test"
 };
 console.log("Hello World from content.");
-
-},{}]},{},[1]);
