625136c4ab301518c9420710d58712786be62897
matthewjmay <mmay51598@gmail.com>
2017-09-16 12:09:15 -0400

    added npm and browserify/babelify

--- extension/content.js
+++ extension/content.js
@@ -1,39 +1,46 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
+
 // this code runs on every page opened
-function scrapeFacebookMessanger(){
+function scrapeFacebookMessanger() {
   var messages = [];
   var chat = document.getElementsByClassName("_aok");
   // find the messages not from the current user.
   var last = true;
-  for (var i = 0;i<chat.length; i++) {
-    if(chat[i].parentElement.dataset.tooltipPosition === "left"){
-      if(last){
+  for (var i = 0; i < chat.length; i++) {
+    if (chat[i].parentElement.dataset.tooltipPosition === "left") {
+      if (last) {
         messages.push([chat[i].textContent]);
-      } else{
-        messages[messages.length-1].push(chat[i].textContent);
+      } else {
+        messages[messages.length - 1].push(chat[i].textContent);
       }
       last = false;
-    } else{
+    } else {
       last = true;
     }
   };
-  return {"messages":messages};
+  return { "messages": messages };
 }
-function scrapeGmail(){
+function scrapeGmail() {
   var exp = /#inbox\/[a-zA-Z0-9]+/;
   // check to see that they are in fact viewing an email, not just at their inbox
-  if(window.location.href.toString().match(exp) == null){return}
-  // grab the data
-  const from = document.getElementsByClassName("gD")[0].dataset.hovercardId;
-  const subject = document.getElementsByClassName("hP")[0].textContent;
-  const content = document.getElementsByClassName("aXjCH").textContent;
-  const pkg = {
-    "from" : from,
-    "subject" : subject,
-    "messages" : [content]
+  if (window.location.href.toString().match(exp) == null) {
+    return;
   }
-  return pkg
+  // grab the data
+  var from = document.getElementsByClassName("gD")[0].dataset.hovercardId;
+  var subject = document.getElementsByClassName("hP")[0].textContent;
+  var content = document.getElementsByClassName("aXjCH").textContent;
+  var pkg = {
+    "from": from,
+    "subject": subject,
+    "messages": [content]
+  };
+  return pkg;
 }
-const index = {
-  "mail.google.com" : 
-}
-console.log("Hello World from content.")
+var index = {
+  "mail.google.com": "test"
+};
+console.log("Hello World from content.");
+
+},{}]},{},[1]);
