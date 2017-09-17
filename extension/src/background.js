50970e36c40625475272b50277dc94e5f51b8f89
matthewjmay <mmay51598@gmail.com>
2017-09-17 00:29:06 -0400

    fixed bugs

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,6 +1,6 @@
 // this code can run in the background, but is currently set to work with events.
 // Google: chrome.runtime.onMessage.addListener
-const HOST = "127.0.0.1"
+const HOST = "localhost"
 const PORT = "8080"
 const $ = require('jquery');
 
