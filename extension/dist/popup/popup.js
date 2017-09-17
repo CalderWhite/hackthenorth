9e8bbebd59f126b54d945f541dd5e163be5bc83b
Dhyan Pathak <dhyan.pathak@gmail.com>
2017-09-17 03:00:40 -0400

    Report sending UI added

--- extension/dist/popup/popup.js
+++ extension/dist/popup/popup.js
@@ -8,33 +8,42 @@ function showMsg() {
 	}, 3000);
 }
 
-var x, y = null;
-	
-function handleWindowClose(e) {
-   	e = window.event || e;
- 	x = event.clientX;
-   	y = event.clientY; 
+/*
+function mouseCoordinate(e) {
+    var x = e.pageX;
+    var y = e.pageY;
+    var el = document.elementFromPoint(x, y);
+		console.log(el);
+    //alert("User clicked at position (" + x + "," + y + ")")
+};
+
+
+
+document.onmousemove = function(e) {
+	var x = e.pageX;
+    var y = e.pageY;
+    var el = document.elementFromPoint(x, y);
+		console.log(el);
 }
-
-document.addEventListener('DOMContentLoaded', function() {
+*/
+document.addEventListener('DOMContentLoaded', function(e) {
 	var button = document.getElementById("picker");
-	
-	button.addEventListener('click', function() {
-		document.getElementById("msg").innerHTML = "Hover over a malicious link and press A";
-
+	var summary = document.getElementById("summary");
+	var link = document.getElementById("link");
+	button.addEventListener('click', function(e) {
+		var descrip = summary.value;
+		var url = link.value;
+		document.getElementById("msg").innerHTML = "Processing your report...";
 		setTimeout(function(){
-	    document.getElementById("msg").innerHTML = '';
-	}, 3000);
+	    	document.getElementById("msg").innerHTML = '';
+		}, 3000);
+				
 	});
-	
 
-
-	button.addEventListener('keypress', function() {
-		//var x = event.clientX, y = event.clientY,
-	    window.onbeforeunload = handleWindowClose;
-	    elementMouseIsOver = document.elementFromPoint(x, y);
-	    console.log(elementMouseIsOver);
-	    console.log(x, y);
-	})	
+	button.addEventListener('keypress', function(e) {
+	   	const tab = chrome.tabs.query({ active: true, currentWindow: true })[0];
+		console.log(tab);
+	   	invisibleBtn.click();
+	});	
 
 })
