db9ea198a89a43c4e15bfd3b7e78a31b2148f2cd
matthewjmay <mmay51598@gmail.com>
2017-09-16 22:09:49 -0400

    added cymon req

--- server/main.js
+++ server/main.js
@@ -32,36 +32,47 @@ router.get('/', function(req, res) {
 app.use('/api', router);
 
 router.route('/')
+	.post(getSiteRatings);
 
-	.post(function(req, res) {
-		let count = req.links.length;
-		req.links.forEach((link, i) => {
-			getIpAddress(link, (ipAddr) => {
-				genStatusArray(ipAddr, i)
-			});
+function getSiteRatings(req, res) {
+	let total = req.links.length;
+	let count = 0;
+	req.links.forEach((link, i) => {
+		getIpAddress(link, (ipAddr) => {
+			genStatusArray(ipAddr, i);
+	});
+	res.json({ message: "Success!"});
+
+	function getIpAddress(origurl) {
+		const options = {  
+		    url: origurl,
+		    method: 'HEAD',
+		    headers: {
+		        'Accept': 'application/json',
+		        'Accept-Charset': 'utf-8',
+		        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.16 (KHTML, like Gecko) Chrome/24.0.1304.0 Safari/537.16'
+		    }
+		}
+
+		request(options, function (err, res, body) {
+			if (res.headers.refresh === 'undefined') {
+				
+			}
 		});
-		res.json({ message: "Success!"});
-	})
+	}
+
+	function genStatusArray(ipAddr, i) {
+		request.get(`https://api.cymon.io/v2/ioc/search/ip/${ipAddr}`)
+  		.on('response', function(response) {
+    	 	if (response.statusCode === 200) {
+    	 		if (response.body)
+    	 	}
+    		console.log(response.headers['content-type']) // 'image/png'
+  	})
+	}
+})
 
 // START THE SERVER
 // =============================================================================
 app.listen(port);
 console.log('Server running on port ' + port);
-
-function getIpAddress(origurl) {
-	const options = {  
-	    url: origurl,
-	    method: 'HEAD',
-	    headers: {
-	        'Accept': 'application/json',
-	        'Accept-Charset': 'utf-8',
-	        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.16 (KHTML, like Gecko) Chrome/24.0.1304.0 Safari/537.16'
-	    }
-	}
-
-	request(options, function (err, res, body) {
-		if (res.headers.refresh === 'undefined') {
-			
-		}
-	});
-}
